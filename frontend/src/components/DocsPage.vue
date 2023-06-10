6c1c49ca017e6d34cef56ec6af226df1594b2bed
veronike <veronika@baier.cz>
2023-06-10 15:23:26 +0200

    docs page first push

---
+++ frontend/src/components/DocsPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Inputs</div>
+  <div>CODES
+    <div>code</div>
+    <div>style</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "docs"
+}
+</script>
+
+<style scoped>
+
+</style>
